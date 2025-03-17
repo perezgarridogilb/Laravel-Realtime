ac4498245d0d165a464b538fc08d3a2403edac15
Gilberto <gilberto@teknik.mx>
2025-03-17 15:12:23 -0600

    Displaying real-time notifications using Laravel Echo

--- LaravelRealtime/resources/js/app.js
+++ LaravelRealtime/resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+Echo.channel('notifications')
+    .listen('UserSessionChanged', (e) => {
+        const notificactionElement = document.getElementById('notification');
+
+        notificactionElement.innerText = e.message;
+        
+        notificactionElement.classList.remove('invisible');
+        notificactionElement.classList.remove('alert-success');
+        notificactionElement.classList.remove('alert-danger');
+
+        notificactionElement.classList.add('alert-' + e.type);
+    });
